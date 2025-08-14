a280cebfdb98b3ac9c2b65c99ed1db38138843e5
hamzanazir27 <hamzaramay51@gmail.com>
2025-08-14 06:39:03 +0500

    commponnentc check for added in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import TopDoctors from "./components/TopDoctors";
+import Reviews from "./components/Reviews";
+import Footer from "./components/Footer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
+    <HeroSection />
+    <TopDoctors />
+    <Reviews />
+    <Footer />
   </StrictMode>
 );
